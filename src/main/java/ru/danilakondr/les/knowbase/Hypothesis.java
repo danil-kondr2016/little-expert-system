62c5b0df486a8c705f231bc17c20b018670c2fbc
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-24 19:53:21 +0600

    Hypothesis

---
+++ src/main/java/ru/danilakondr/les/knowbase/Hypothesis.java
@@ -0,0 +1,27 @@
+package ru.danilakondr.les.knowbase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hypothesis {
+    private final String name;
+
+    private final Map<Integer, ProbabilityPair> answers;
+
+    public Hypothesis(String name) {
+        this.name = name;
+        this.answers = new HashMap<>();
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void putAnswer(int question, float yes, float no) {
+        answers.put(question, new ProbabilityPair(yes, no));
+    }
+
+    public ProbabilityPair getAnswer(int question) {
+        return answers.get(question);
+    }
+}
