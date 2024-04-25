da367b00d1f8850267cd4eab989934d29a49bcca
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-25 20:51:13 +0600

    App: тестовая обработка

--- src/main/java/ru/danilakondr/les/App.java
+++ src/main/java/ru/danilakondr/les/App.java
@@ -1,13 +1,44 @@
 package ru.danilakondr.les;
 
-/**
- * Hello world!
- *
- */
+import ru.danilakondr.les.knowbase.KnowledgeBase;
+import ru.danilakondr.les.knowbase.ProbabilityPair;
+import ru.danilakondr.les.parser.MKBParser;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App 
 {
+    private static final String MKB_TEST =
+            "The internal test file. Created at 2024-04-25\r\n" +
+                    "\r\n" +
+                    "Questions:\r\n" +
+                    "Question 1\r\n" +
+                    "Question 2\r\n" +
+                    "\r\n" +
+                    "Hypothesis 1, 0.5, 1,1,0, 2,0,1\r\n" +
+                    "Hypothesis 2, 0.5, 1,0,1, 2,1,0\r\n";
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(MKB_TEST);
+        MKBParser parser = new MKBParser(sc);
+        KnowledgeBase kbTest = parser.parse();
+
+        System.out.printf("Comment: %s%n", kbTest.getComment());
+        System.out.println("Questions: ");
+        kbTest.getQuestions().forEach((x) -> System.out.println("  " + x));
+        System.out.println("Hypotheses: ");
+        kbTest.getHypotheses().forEach((h) -> {
+            System.out.println("  Name: " + h.name());
+            System.out.println("  Probability a priori: " + h.pApriori());
+            System.out.println("  Question references: ");
+            for (int i = 0; i < kbTest.getQuestions().size(); i++) {
+                ProbabilityPair p = h.getAnswer(i);
+                if (p == null)
+                    continue;
+                System.out.printf(Locale.US, "    %d: y=%f; n=%f%n", i, p.yes(), p.no());
+            }
+        });
     }
 }
