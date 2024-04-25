453217e81fb444c9152d64810789d40725b42cce
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-25 21:09:09 +0600

    ProbabilityPair: javadoc

--- src/main/java/ru/danilakondr/les/knowbase/ProbabilityPair.java
+++ src/main/java/ru/danilakondr/les/knowbase/ProbabilityPair.java
@@ -2,18 +2,33 @@ package ru.danilakondr.les.knowbase;
 
 import java.util.Objects;
 
+/**
+ * Пара вероятностей: вероятность P<sub>y</sub> = P(E/Y) и P<sub>n</sub> =
+ * P(E/!Y).
+ */
 public class ProbabilityPair {
     private final float yes, no;
 
+    /**
+     * Конструктор пары вероятностей.
+     * @param yes вероятность "да" (P(E/Y))
+     * @param no вероятность "нет" (P(E/!Y))
+     */
     public ProbabilityPair(float yes, float no) {
         this.yes = yes;
         this.no = no;
     }
 
+    /**
+     * Получить вероятность "да".
+     */
     public float yes() {
         return yes;
     }
 
+    /**
+     * Получить вероятность "нет".
+     */
     public float no() {
         return no;
     }
