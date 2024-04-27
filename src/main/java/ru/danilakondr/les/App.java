cf27a9c1018047df2e7449c73b2c2d6608b3887d
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-27 20:57:19 +0600

    App: проверка базы знаний из МЭС 2.0

--- src/main/java/ru/danilakondr/les/App.java
+++ src/main/java/ru/danilakondr/les/App.java
@@ -9,15 +9,32 @@ import java.util.Scanner;
 
 public class App 
 {
+    /*
+     * Для теста была имплантирована база знаний по грибам из стандартной поставки
+     * Малой экспертной системы 2.0.
+     */
     private static final String MKB_TEST =
-            "The internal test file. Created at 2024-04-25\r\n" +
-                    "\r\n" +
-                    "Questions:\r\n" +
-                    "Question 1\r\n" +
-                    "Question 2\r\n" +
-                    "\r\n" +
-                    "Hypothesis 1, 0.5, 1,1,0, 2,0,1\r\n" +
-                    "Hypothesis 2, 0.5, 1,0,1, 2,1,0\r\n";
+            "Пример базы знаний о грибах.\n" +
+            "Автор Роман Бухнин.\n" +
+            "Данная база знаний является далеко не полноценной и служит исключительно для демонстрационных целей.\n" +
+            "\n" +
+            "Признаки:\n" +
+            "Гриб пластинчатый?\n" +
+            "Гриб полностью жёлтый?\n" +
+            "У гриба прочная толстая ножка?\n" +
+            "Гриб полностью ярко-коричневый?\n" +
+            "Шляпка красная?\n" +
+            "Есть ли венчик на ножке?\n" +
+            "\n" +
+            "Лисичка,0.05, 1,1,0.5, 2,0.9,0.1, 3,0.03,0.3, 4,0,0.1, 5,0,0.25, 6,0,0.35\n" +
+            "Свинух,0.05, 1,1,0.5, 2,0.005,0.2, 4,1,0.01, 5,0,0.25, 6,0,0.35\n" +
+            "Подберёзовик,0.05, 1,0,0.5, 2,0,0.2, 3,0.25,0.3, 4,0.1,0.1, 5,0,0.25, 6,0,0.35\n" +
+            "Подосиновик,0.05, 1,0,0.5, 2,0,0.2, 3,0.98,0.15, 4,0,0.1, 5,0.8,0.22, 6,0,0.35\n" +
+            "Белый Гриб,0.05, 1,0,0.5, 2,0,0.2, 3,1,0.15, 4,0.001,0.1, 5,0.05,0.5, 6,0,0.35\n" +
+            "Опёнок,0.05, 1,1,0.5, 2,0.7,0.15, 3,0,0.3, 4,0.15,0.1, 5,0,0.25\n" +
+            "Мухомор Красный,0.05, 1,1,0.5, 2,0,0.2, 3,0.12,0.3, 4,0,0.1, 5,1,0.2, 6,1,0.3\n" +
+            "Сыроежка,0.1, 1,1,0.5, 2,0.02,0.2, 3,0.1,0.3, 4,0,0.1, 5,0.3,0.2, 6,0,0.35\n" +
+            "Маслёнок,0.05, 1,0,0.5, 2,0.25,0.2, 3,0.02,0.3, 4,0.05,0.1, 5,0.001,0.25, 6,0.7,0.3";
 
     public static void main( String[] args )
     {
@@ -34,11 +51,34 @@ public class App
             System.out.println("  Prior probability: " + h.pPrior());
             System.out.println("  Question references: ");
             for (int i = 0; i < kbTest.getQuestions().size(); i++) {
-                ProbabilityPair p = h.getAnswer(i);
+                ProbabilityPair p = h.getAnswerPair(i);
                 if (p == null)
                     continue;
                 System.out.printf(Locale.US, "    %d: y=%f; n=%f%n", i, p.yes(), p.no());
             }
         });
+
+        System.out.println("This KB will be executed.");
+        LittleExpertSystem les = new LittleExpertSystem();
+        les.loadKnowledgeBase(kbTest);
+
+        Scanner cin = new Scanner(System.in);
+        les.start();
+        while (les.isRunning()) {
+            int q = les.getCurrentQuestion();
+            String qString = kbTest.getQuestions().get(q);
+
+            float[] values = les.getValues();
+            for (int i = 0; i < values.length; i++)
+                System.out.printf("%s: %f%n", kbTest.getHypotheses().get(i).name(), values[i]);
+
+            System.out.printf("Q %d: %s%n", q, qString);
+            float level = cin.nextFloat();
+            les.answer(level);
+        }
+
+        float[] values = les.getValues();
+        for (int i = 0; i < values.length; i++)
+            System.out.printf("%s: %f%n", kbTest.getHypotheses().get(i).name(), values[i]);
     }
 }
