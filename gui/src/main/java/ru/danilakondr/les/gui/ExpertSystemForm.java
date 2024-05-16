5bedecf5cd7c299b339f8d07a183d4301f90fd32
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-05-16 20:38:36 +0600

    gui: ExpertSystemForm

---
+++ gui/src/main/java/ru/danilakondr/les/gui/ExpertSystemForm.java
@@ -0,0 +1,18 @@
+package ru.danilakondr.les.gui;
+
+import javax.swing.*;
+
+public class ExpertSystemForm {
+    private JButton btnOpen;
+    private JButton btnSaveProtocol;
+    private JButton btnConf;
+    private JTextPane tpComment;
+    private JList lstResults;
+    private JList lstProtocol;
+    private JButton btnStartStop;
+    private JTextField tfConfidenceValue;
+    private JTextPane tpQuestion;
+    private JList<String> lstQuestions;
+    private JButton btnSendAnswer;
+
+}
